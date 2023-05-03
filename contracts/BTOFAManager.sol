6fbe7496068d7d7450c8bd1064197d1e68d6b2ec
Pastacom <mailkiborg@mail.ru>
2023-05-03 22:15:09 +0300

    added new checkers and implemented batch transfer

--- contracts/BTOFAManager.sol
+++ contracts/BTOFAManager.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IBTOFAToken is IERC721 {
     function isExpired(uint256 tokenId) external view returns (bool);
@@ -15,7 +15,6 @@ interface IBTOFAToken is IERC721 {
 }
 
 contract BankManager is IERC721Receiver, Ownable {
-    using SafeMath for uint256;
 
     IERC20 private _BTOFACurrency;
     IBTOFAToken private _BTOFAToken;
@@ -25,20 +24,44 @@ contract BankManager is IERC721Receiver, Ownable {
         _BTOFAToken = IBTOFAToken(token);
     }
 
-    function buyToken(uint256 tokenId) external {
-        uint256 price = _BTOFAToken.getPrice(tokenId);
+    function buyToken(uint256 startId, uint256 endId) external {
+        require(startId <= endId, "BANK MANAGER: Right ID's bound is less than left one.");
+        uint256 price = 0;
+        
+        for (uint256 tokenId = startId; tokenId <= endId; ++tokenId) {
+            price += _BTOFAToken.getPrice(tokenId);
+            require(msg.sender != _BTOFAToken.ownerOf(tokenId),
+                    string(abi.encodePacked("BTOT: User already is an owner of token with ID ", Strings.toString(tokenId), ".")));
+        }
+
         require(_BTOFACurrency.balanceOf(msg.sender) >= price, "BTOC: Insufficient amount of tokens.");
+        require(_BTOFACurrency.allowance(msg.sender, address(this)) >= price, "BTOC: Insufficient allowed amount of tokens to spend.");
         _BTOFACurrency.transferFrom(msg.sender, owner(), price);
-        _BTOFAToken.safeTransferFrom(owner(), msg.sender, tokenId);
+        
+        for (uint256 tokenId = startId; tokenId <= endId; ++tokenId) {
+            _BTOFAToken.safeTransferFrom(owner(), msg.sender, tokenId);
+        }
     }
 
-    function expireToken(uint256 tokenId) public onlyOwner {
-        require(_BTOFAToken.isExpired(tokenId), "BTOT: Token with such ID hasn't expired yet.");
-        uint256 price = _BTOFAToken.getProfit(tokenId);
+    function expireToken(uint256 startId, uint256 endId) public onlyOwner {
+        uint256 price = 0;
+        require(startId <= endId, "BANK MANAGER: Right ID's bound is less than left one.");
+        
+        for (uint256 tokenId = startId; tokenId <= endId; ++tokenId) {
+            price += _BTOFAToken.getProfit(tokenId);
+            require(owner() != _BTOFAToken.ownerOf(tokenId),
+                    string(abi.encodePacked("BTOT: User already is an owner of token with ID ", Strings.toString(tokenId), ".")));
+            require(_BTOFAToken.isExpired(tokenId), "BTOT: Token with such ID hasn't expired yet.");
+        }
+
         require(_BTOFACurrency.balanceOf(owner()) >= price, "BTOC: Insufficient amount of tokens.");
-        _BTOFACurrency.transferFrom(owner(), _BTOFAToken.ownerOf(tokenId), price);
-        _BTOFAToken.safeTransferFrom(_BTOFAToken.ownerOf(tokenId), owner(), tokenId);
-        _BTOFAToken.burn(tokenId);
+        require(_BTOFACurrency.allowance(owner(), address(this)) >= price, "BTOC: Insufficient allowed amount of tokens to spend.");
+        _BTOFACurrency.transferFrom(owner(), _BTOFAToken.ownerOf(startId), price);
+        
+        for (uint256 tokenId = startId; tokenId <= endId; ++tokenId) {
+            _BTOFAToken.safeTransferFrom(_BTOFAToken.ownerOf(tokenId), owner(), tokenId);
+            _BTOFAToken.burn(tokenId);
+        }
     }
 
     function onERC721Received(address, address, uint256, bytes calldata) external override pure returns (bytes4) {
